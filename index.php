e6f1a69a3eb65d671dd845dfc5ada5f4c9bffc12
Thibault LUX <tlux@e2r11p4.42.fr>
2018-03-31 20:50:22 +0200

    html front

---
+++ index.php
@@ -0,0 +1,88 @@
+<?php
+?>
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="index.css">
+</head>
+<table width=100% height =10% border=1>
+	<tr width = 100% height = 10%>
+		<th width = 70% height = 10%> <div> logo / banniere </div>
+		</th>
+		<th width = 15% height = 10%> <div> mon panier </br> </div>
+		</th>
+		<th width = 15% height = 10%> <div> register <br /> sign in </br> </div>
+		</th>
+	</tr>
+</table>
+<table border=1 width = 100% height = 80%>
+	<tr>
+		<th width=15%>
+			<table border=1 width = 100% height = 100%>
+				<tr>
+					<th>
+						<a href="ps4products.html">
+						<IMG src="resources/support/ps4_icon.png" width = 70% height = 50%>
+						</a>
+					</th>
+				</tr>
+				<tr>
+					<th>
+						<a href="switchproducts.html">
+						<IMG src="resources/support/switch_icon.png" width = 70% height = 50%>
+						</a>
+					</th>
+				</tr>
+				<tr>
+					<th>
+						<a href="xboxproducts.html">
+						<IMG src="resources/support/xboxone_icon.png" width = 70% height = 50%>
+						</a>
+					</th>
+				</tr>
+				<tr height=50%>
+				</tr>
+			</table>
+		</th>
+		<th>
+		<table border = 1 width = 100% height = 100%>
+			<tr width = 85% height = 14%>
+				<th  colspan = 4 width = 25% height = 25%>
+					jeux du moment :
+				</th>
+			</tr>
+			<tr width = 85% height = 14%>
+				<th width = 25% height = 25%>
+				</th>
+				<th width = 25% height = 25%>
+				</th>
+				<th width = 25% height = 25%>
+				</th>
+				<th width = 25% height = 25%>
+				</th>
+			</tr>
+			<tr width = 85% height = 14%>
+				<th width = 25% height = 25%>
+				</th>
+				<th width = 25% height = 25%>
+				</th>
+				<th width = 25% height = 25%>
+				</th>
+				<th width = 25% height = 25%>
+				</th>
+			</tr>
+			<tr width = 85% height = 14%>
+				<th width = 25% height = 25%>
+				</th>
+				<th width = 25% height = 25%>
+				</th>
+				<th width = 25% height = 25%>
+				</th>
+				<th width = 25% height = 25%>
+				</th>
+			</tr>
+
+		</table>
+		</th>
+	</tr>
+</table>
+</html>
