8902bd91950b35adb16a54d1dd7bba9fc366c7aa
Thibault LUX <tlux@e2r11p4.42.fr>
2018-03-31 22:45:14 +0200

    new format

--- index.php
+++ index.php
@@ -1,88 +1,78 @@
-<?php
-?>
 <html>
-<head>
-<link rel="stylesheet" type="text/css" href="index.css">
-</head>
-<table width=100% height =10% border=1>
-	<tr width = 100% height = 10%>
-		<th width = 70% height = 10%> <div> logo / banniere </div>
-		</th>
-		<th width = 15% height = 10%> <div> mon panier </br> </div>
-		</th>
-		<th width = 15% height = 10%> <div> register <br /> sign in </br> </div>
-		</th>
-	</tr>
-</table>
-<table border=1 width = 100% height = 80%>
-	<tr>
-		<th width=15%>
-			<table border=1 width = 100% height = 100%>
-				<tr>
-					<th>
-						<a href="ps4products.html">
-						<IMG src="resources/support/ps4_icon.png" width = 70% height = 50%>
-						</a>
-					</th>
-				</tr>
-				<tr>
-					<th>
-						<a href="switchproducts.html">
-						<IMG src="resources/support/switch_icon.png" width = 70% height = 50%>
-						</a>
-					</th>
-				</tr>
-				<tr>
-					<th>
-						<a href="xboxproducts.html">
-						<IMG src="resources/support/xboxone_icon.png" width = 70% height = 50%>
-						</a>
-					</th>
-				</tr>
-				<tr height=50%>
-				</tr>
-			</table>
-		</th>
-		<th>
-		<table border = 1 width = 100% height = 100%>
-			<tr width = 85% height = 14%>
-				<th  colspan = 4 width = 25% height = 25%>
-					jeux du moment :
-				</th>
-			</tr>
-			<tr width = 85% height = 14%>
-				<th width = 25% height = 25%>
-				</th>
-				<th width = 25% height = 25%>
-				</th>
-				<th width = 25% height = 25%>
-				</th>
-				<th width = 25% height = 25%>
-				</th>
-			</tr>
-			<tr width = 85% height = 14%>
-				<th width = 25% height = 25%>
-				</th>
-				<th width = 25% height = 25%>
-				</th>
-				<th width = 25% height = 25%>
-				</th>
-				<th width = 25% height = 25%>
-				</th>
-			</tr>
-			<tr width = 85% height = 14%>
-				<th width = 25% height = 25%>
-				</th>
-				<th width = 25% height = 25%>
-				</th>
-				<th width = 25% height = 25%>
-				</th>
-				<th width = 25% height = 25%>
-				</th>
-			</tr>
+	<head>
+		<title>Index</title>
+		<link rel="stylesheet" href="index.css">
+	</head>
+	<body>
+	<div id="logo">
+		<img src="resources/logo.png">
+		<div id="account">
+			Connexion Inscription
+		</div>
+		<div id="account">
+			Mon panier
+		</div>
+	</div>
+	<div id="categories">
+Categories :
+<br />
+<div id="support">
+	<a href="switchproducts.html"><img id="mini" src="resources/support/xboxone_icon.png"></a>
+<br />XBOX ONE 
+</div> <br />
+<div id="support">
+	<a href="switchproducts.html"><img id="mini" src="resources/support/ps4_icon.png"></a>
+<br />PLAYSTATION 4 
+</div> <br />
+<div id="support">
+	<a href="switchproducts.html"><img id="mini" src="resources/support/switch_icon.png"></a>
+<br />NINTENDO SWITCH 
+</div> <br />
+</div>
+<div class="scroller">
+	<div class="product">
+	<img class="image" src="resources/games/switch/1.jpeg">
+	<div class="middle">
+	<div class="text">ajouter au panier </div>
+	</div>
+	</div>
+<div class="product">
+	<img class="image" src="resources/games/switch/1.jpeg">
+	<div class="middle">
+	<div class="text">ajouter au panier </div>
+	</div>
+	</div>
+<div class="product">
+	<img class="image" src="resources/games/switch/1.jpeg">
+	<div class="middle">
+	<div class="text">ajouter au panier</div>
+	</div>
+	</div>
+<div class="product">
+	<img class="image" src="resources/games/switch/1.jpeg">
+	<div class="middle">
+	<div class="text">ajouter au panier </div>
+	</div>
+	</div>
+<div class="product">
+	<img class="image" src="resources/games/switch/1.jpeg">
+	<div class="middle">
+	<div class="text">ajouter au panier </div>
+	</div>
+	</div>
+<div class="product">
+	<img class="image" src="resources/games/switch/1.jpeg">
+	<div class="middle">
+	<div class="text">ajouter au panier </div>
+	</div>
+	</div>
+<div class="product">
+	<img class="image" src="resources/games/switch/1.jpeg">
+	<div class="middle">
+	<div class="text">ajouter au panier </div>
+	</div>
+	</div>
 
-		</table>
-		</th>
-	</tr>
-</table>
+</div>
+
 </html>
